bd2e775d8d4e2e911f29a993dfc9a369e2b3b560
Deathspike <meister.deathspike@outlook.com>
2022-01-14 09:03:26 +0100

    Add mutex to prevent running multiple instances.

--- FullscreenBrowserOverlay/Program.cs
+++ FullscreenBrowserOverlay/Program.cs
@@ -7,9 +7,12 @@ namespace FullscreenBrowserOverlay
         [STAThread]
         public static void Main()
         {
+            using var mutex = new Mutex(true, "{7675EE47-C9A1-4C62-BF13-D76B1343E9CA}");
+            if (!mutex.WaitOne(TimeSpan.Zero, true)) return;
             var store = Store.LoadOrDefault();
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm(store));
+            mutex.Dispose();
         }
     }
 }
